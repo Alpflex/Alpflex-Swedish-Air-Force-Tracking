2f409c40e40114a6d92e9c7089f37d114b926803
Alpflex <29543573+Alpflex@users.noreply.github.com>
2022-10-12 21:11:21 +0200

    Create myimport.py

---
+++ myimport.py
@@ -0,0 +1,12 @@
+import tweepy
+from opensky_api import OpenSkyApi
+import time
+import functools
+import operator
+import random
+from geopy.geocoders import Nominatim
+
+import folium
+from selenium import webdriver
+import os
+import time
